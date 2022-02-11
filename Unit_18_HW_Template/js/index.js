295b0dc1179282520d196329b10cd2cac955c3be
APuckett <86848741+Gamork@users.noreply.github.com>
2022-02-11 12:41:58 -0500

    attempt at CSS and JS
    added hover states for menu and buttons. Put in some button and a mouse location trigger in JS. but they did not work.

--- Unit_18_HW_Template/js/index.js
+++ Unit_18_HW_Template/js/index.js
@@ -1,1 +1,11 @@
 console.log("Your index.js file is loaded correctly!");
+
+$("#logoAndy").click(function(){
+    alert("ANDY's LOGO!");
+  });
+
+  $(".skillsButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".heading").offset().top},
+        'slow');
+});
