f577a1543318072c970fa0109e1466f09a3711ee
APuckett <86848741+Gamork@users.noreply.github.com>
2022-02-12 09:50:42 -0500

    set head, and english

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 console.log("Your index.js file is loaded correctly!");
 
 $("#logoAndy").click(function(){
-    alert("ANDY's LOGO!");
+    alert("ANDY's LOGO! It is a mash of my name. One day, I would like it to animate by starting with ANDY merging into the logo.");
   });
 
   $(".skillsButton").click(function() {
